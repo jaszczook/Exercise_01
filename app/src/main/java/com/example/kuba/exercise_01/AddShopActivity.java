d1ad1f68875608705f0ad0a40732007197537658
Kuba <jaszczook@gmail.com>
2018-12-20 02:16:05 +0100

    Single import optimized

--- app/src/main/java/com/example/kuba/exercise_01/AddShopActivity.java
+++ app/src/main/java/com/example/kuba/exercise_01/AddShopActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
